291888487856af287b15eed4fb829f23800771c6
DevadharshiniSK <devashanmugaraj43@gmail.com>
2022-07-10 16:52:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const generatePDF = async (name) => {
+    const { PDFDocument, rgb } = PDFLib;   
+    
+    const exBytes = await fetch("./certificate.pdf").then((res)=>{
+      return res.arrayBuffer();    
+    });
+
+
+    const pdfDoc = await PDFDocument.load(exBytes);
+    const pages = pdfDoc.getPages()
+    const pg = pages[0]
+   
+    pg.drawText(name,{
+         x: 55,
+         y: 265,
+         size: 25
+
+    })
+     const uri = await pdfDoc.saveAsBase64({dataUri: true})
+
+    document.querySelector("#coinpdf").src = uri;
+
+
+       
+}
+  
+generatePDF("DEVADHARSHINI S K")
+
+let btnDownload = document.querySelector("button");
+btnDownload.addEventListener("click", startDownload);
+async function startDownload() {
+let url = "certificate.pdf";
+let fileName = "CoinEarner";
+const res = await fetch(url);
+const blob = await res.blob();
+saveAs(blob, fileName);
+}
